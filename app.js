a7a89d7e2de376944999e84ca55ce1dab038c804
Radchenko Artem <ddruming@yandex.ru>
2026-04-16 19:11:33 +0300

    feat: реализовал переключение между режимами работы TodoItem

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 "use strict"
 
 import {Header} from "./components/header/Header.js";
-import {TodoItem} from "./components/item/TodoItem.js";
+import {TodoItem} from "./components/todoItem/TodoItem.js";
 
 
 const app = document.getElementById('app');
 
 const header = new Header()
     .onAddClick(() => {
-        const todoItem = new TodoItem()
-            .setTitle("Заголовок заметки")
-            .setDescription("Текст очень важной заметки");
-
+        const todoItem = new TodoItem();
         todoItem.mount(app);
     });
 header.mount(app);
---
+++ components/todoItem/TodoItem.js
@@ -0,0 +1,112 @@
+"use strict"
+
+
+import {Component} from "../../Component.js";
+import {Button} from "../../ui/button/Button.js";
+import {Checkbox} from "../../ui/checkbox/Checkbox.js";
+
+
+export class TodoItem extends Component {
+    #titleInput = null;
+    #descriptionInput = null;
+
+    #checkbox = new Checkbox()
+        .setIcon("../../assets/check.svg")
+        .onClick(() => console.log("checked"));
+
+    #editButton = new Button()
+        .setIcon("../../assets/edit.svg")
+        .onClick(() => {
+            this.setState({mode: "edit"});
+        });
+
+    #deleteButton = new Button()
+        .setIcon("../../assets/delete.svg")
+        .onClick(() => this.unmount());
+
+    #appendButton = new Button()
+        .setIcon("../../assets/check.svg")
+        .onClick(() => {
+            this.setState({
+                mode: "read",
+                title: this.#titleInput.value,
+                description: this.#descriptionInput.value
+            });
+        });
+
+    state = {
+        mode: "edit",
+        title: "",
+        description: ""
+    };
+
+    setTitle(title) {
+        return this.setState({title});
+    }
+
+    setDescription(description) {
+        return this.setState({description});
+    }
+
+    setMode(mode) {
+        return this.setState({mode: mode});
+    }
+
+    render() {
+        const container = document.createElement("div");
+        container.className = "item-container";
+
+        if (this.state.mode === "edit") {
+            this.#renderEditMode(container);
+        } else {
+            this.#renderReadMode(container);
+        }
+
+        return container;
+    }
+
+    #renderEditMode(container) {
+        const textContainer = document.createElement("div");
+        textContainer.className = "text-container";
+
+        const title = document.createElement("input");
+        title.className = "text-container__title";
+        title.placeholder = "Заголовок";
+        title.value = this.state.title;
+
+        const description = document.createElement("input");
+        description.className = "text-container__description";
+        description.placeholder = "Описание";
+        description.value = this.state.description;
+
+        this.#titleInput = title;
+        this.#descriptionInput = description;
+
+        textContainer.append(title, description);
+        container.append(textContainer);
+
+        this.#appendButton.mount(container);
+        this.#deleteButton.mount(container);
+    }
+
+    #renderReadMode(container) {
+        this.#checkbox.mount(container);
+
+        const textContainer = document.createElement("div");
+        textContainer.className = "text-container";
+
+        const title = document.createElement("h2");
+        title.className = "text-container__title";
+        title.textContent = this.state.title;
+
+        const description = document.createElement("p");
+        description.className = "text-container__description";
+        description.textContent = this.state.description;
+
+        textContainer.append(title, description);
+        container.append(textContainer);
+
+        this.#editButton.mount(container);
+        this.#deleteButton.mount(container);
+    }
+}
