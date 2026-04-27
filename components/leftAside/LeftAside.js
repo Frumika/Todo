bb42bcf2a5432ef18c6da9c26533294d63be4a0f
Radchenko Artem <ddruming@yandex.ru>
2026-04-27 21:25:44 +0300

    feat: чуть заполнил left-aside

--- components/leftAside/LeftAside.js
+++ components/leftAside/LeftAside.js
@@ -2,6 +2,7 @@
 
 
 import {Component} from "../../ui/Component.js";
+import {Button} from "../../ui/button/Button.js";
 
 export class LeftAside extends Component{
 
@@ -12,6 +13,26 @@ export class LeftAside extends Component{
         const leftAsideContainer = document.createElement("div");
         leftAsideContainer.className = "left-aside__container";
 
+        const auth = document.createElement("div");
+        auth.className = "auth"
+
+        const loginButton = new Button()
+            .setIcon("../../assets/login.svg")
+            .setText("Вход")
+            .setFontColor("#202020")
+            .setBackColor("#FCFAF8")
+            .hasActiveBackground()
+        loginButton.mount(auth);
+
+        const registerButton = new Button()
+            .setIcon("../../assets/register.svg")
+            .setText("Регистрация")
+            .setFontColor("#202020")
+            .setBackColor("#FCFAF8")
+            .hasActiveBackground()
+        registerButton.mount(auth);
+
+        leftAsideContainer.append(auth);
         leftAside.append(leftAsideContainer);
 
         return leftAside;
